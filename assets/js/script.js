a6b7b0380a7906cf94dbfc0e02886fbe5702343f
Dawson Filion <Dawsonfilion21@gmail.com>
2022-03-13 19:09:45 -0400

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var startButton = document.querySelector(".start-button");
+
+startButton.addEventListener("click", function(){
+    document.querySelector(".center").style.display = "none";
+})
+
+
+
