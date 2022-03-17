87822b4924617b7caa682cfdccf44530c9774509
Dawson Filion <Dawsonfilion21@gmail.com>
2022-03-16 22:11:21 -0400

    progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,103 +1,51 @@
 var startButton = document.querySelector(".start-button");
 var questions = [
     {
-        questionTitle: 'Commonly used data types DO Not include_______.',
+        questionTitle: "Commonly used data types DO Not include_______.", 
         choices: ["string", "booleans", "alerts", "numbers"]
+    },
+    {
+        questionTitle: "The condition in an if / else statement is enclosed with _____.",
+        choices: ["quotes", "curly brackets", "parenthesis", "square brackets"]
+    },
+    {
+        questionTitle: "Arrays in JavaScript can be used to store _______.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"]
+    },
+    {
+        questionTitle: "String values must be enclosed within _______ when being assigned to variables",
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"]
+    },
+    {
+        questionTitle: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["JavaScript", "terminal/bash", "for loops", "console.log"]
     }
+          
 ]
-var newQuestSection = document.querySelector(".newQuest0");
-
+var newQuestSection = document.querySelector(".question");
+var qNumber = 0;
 
 startButton.addEventListener("click", function(){
     document.querySelector(".center").style.display = "none";
-    newQuest1()
+    makeQuestion(qNumber);
 })
 
-var  newQuest1 = function () {
-    var quest0 = document.querySelector(".question");
-    quest0.textContent = questions[0].questionTitle 
+function makeQuestion (questionNumber) {
+    var quest0 = document.querySelector("div.newQuest0 > h2");
+    quest0.textContent = questions[questionNumber].questionTitle; 
  
     for (var i=0; i < 4; i++) {
         var choiceButton = document.createElement("button");
-        choiceButton.textContent = questions[0].choices[i];
-        choiceButton.addEventListener("click", function(){
-            document.querySelector(".newQuest0").style.display = "none";
+        choiceButton.textContent = questions[questionNumber].choices[i];
+        choiceButton.addEventListener("click",   function(){
+            newQuestSection.textContent = "";
+            makeQuestion(qNumber);
         })
-        newQuestSection.appendChild(choiceButton)
-    }
+        newQuestSection.appendChild(choiceButton);
+    } 
+    qNumber += 1;
 }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//  const myQuestions = [
-//     {
-
-//     }
-// ]
-//         question: "Commonly used data types DO Not include:",
-//         answers: {
-//             a: "strings",
-//             b: "booleans",
-//             c: "alerts",
-//             d: "numbers"
-//         },
-//         correctAnswer: "c"
-//     },
-//     {
-//         question: "The condition in an if/ else statement is enclosed with_______.",
-//         answers: {
-//             a: "quotes",
-//             b: "curly brackets",
-//             c: "parenthesis",
-//             d: "square brackets"
-//         },
-//         correctAnswer: "c" 
-//     },
-    
-    
-// ]
 
