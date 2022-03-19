03cf7f3e9e2b409c508de260b39096c32ead791b
Dawson Filion <Dawsonfilion21@gmail.com>
2022-03-19 13:06:33 -0400

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,43 @@
 var currentQuestion = -1;
+var answer1 = document.querySelector("#answer1");
+var answer2 = document.querySelector("#answer2");
+var answer3 = document.querySelector("#answer3");
+var answer4 = document.querySelector("#answer4");
+var ans = [answer1, answer2, answer3, answer4];
+var currentButton = 0;
 var userAnswers = [];
 var startButton = document.querySelector(".start-quiz");
 var questions = [
     {
         questionTitle: "Commonly used data types DO Not include_______.", 
         choices: ["string", "booleans", "alerts", "numbers"],
-        answer: 2
+        answer: "alerts"
     },
     {
         questionTitle: "The condition in an if / else statement is enclosed with _____.",
         choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
-        answer: 2
+        answer: "parenthesis"
     },
     {
         questionTitle: "Arrays in JavaScript can be used to store _______.",
         choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-        answer: 3
+        answer: "all of the above"
     },
     {
         questionTitle: "String values must be enclosed within _______ when being assigned to variables",
         choices: ["commas", "curly brackets", "quotes", "parenthesis"],
-        answer: 2
+        answer: "quotes"
     },
     {
         questionTitle: "A very useful tool used during development and debugging for printing content to the debugger is:",
         choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
-        answer: 3
+        answer: "console.log"
     }  
 ]
 
 function showQuestion (question) {
     var h2 = document.querySelector("#question > h2");
     h2.textContent = question.questionTitle;
-
-    var answer1 = document.querySelector("#answer1");
-    var answer2 = document.querySelector("#answer2");
-    var answer3 = document.querySelector("#answer3");
-    var answer4 = document.querySelector("#answer4");
 
     answer1.textContent = question.choices[0];
     answer2.textContent = question.choices[1];
@@ -46,16 +47,13 @@ function showQuestion (question) {
     var questionDiv = document.querySelector("#question");
         
     questionDiv.style.display = "flex";
-    var ans = [answer1, answer2, answer3, answer4];
     for(var i = 0; i < 4; i++){
-        var temp = ans[i];
-        ans[i].removeEventListener("click", )
-        ans[i].addEventListener("click", function(){
-            console.log(i);
-            userChoiceAction(temp);
-        });
-    }
+        ans[i].addEventListener("click", clickAnswer);
+       
+    };
 
+    currentButton = 0;
+    console.log(userAnswers);
 }
 
 function nextQuestion (){
@@ -69,14 +67,34 @@ startButton.addEventListener("click", function (){
 });
 
 function userChoiceAction (answerButton){
-    console.log(answerButton);
-    userAnswers.push(answerButton.textContent);
-    nextQuestion();
+    console.log(currentQuestion);
+    if (currentQuestion >= questions.length - 1) {
+        showScore()
+    }else{
+        userAnswers.push(answerButton.textContent);
+        nextQuestion();
+    }
 }
 
+function clickAnswer () {
+    console.log("button" + currentButton);
+    currentButton++;
+    userChoiceAction (ans[currentButton]);
+}
 
-
-
+function showScore () {
+    var questionDiv = document.querySelector("#question");
+    questionDiv.remove();
+    var score = 0;
+    for(var i = 0; i < questions.length; i++) {
+        if (questions[i].answer===userAnswers[i]){
+            score = score + 5;
+        }
+        console.log(questions[i].answer + " - " + userAnswers[i]);
+    } 
+    var scorePage = document.querySelector(".score-page");
+    scorePage.innerHTML += "Your score is " + score;
+}
 
 
 
