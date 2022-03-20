8eca590b2680c399e7e28145b9da56ff9b7d9bb7
Dawson Filion <Dawsonfilion21@gmail.com>
2022-03-20 14:36:32 -0400

    done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,22 @@
 var currentQuestion = -1;
+var initials = "";
+var score = 0;
+
+// localStorage.setItem("")
+
 var answer1 = document.querySelector("#answer1");
 var answer2 = document.querySelector("#answer2");
 var answer3 = document.querySelector("#answer3");
 var answer4 = document.querySelector("#answer4");
+
 var ans = [answer1, answer2, answer3, answer4];
+
 var currentButton = 0;
 var userAnswers = [];
 var startButton = document.querySelector(".start-quiz");
+
+var scorePage = document.querySelector(".score-page");
+
 var questions = [
     {
         questionTitle: "Commonly used data types DO Not include_______.", 
@@ -47,53 +57,103 @@ function showQuestion (question) {
     var questionDiv = document.querySelector("#question");
         
     questionDiv.style.display = "flex";
-    for(var i = 0; i < 4; i++){
-        ans[i].addEventListener("click", clickAnswer);
-       
-    };
-
-    currentButton = 0;
-    console.log(userAnswers);
+    
+    removeEventListeners();
+    
+    answer1.addEventListener("click", clickAnswer1);
+    answer2.addEventListener("click", clickAnswer2);
+    answer3.addEventListener("click", clickAnswer3);
+    answer4.addEventListener("click", clickAnswer4);
 }
 
 function nextQuestion (){
     document.querySelector(".center").style.display = "none";
-    currentQuestion = currentQuestion + 1;
+    currentQuestion++;
     showQuestion (questions [currentQuestion]); 
 }
 
-startButton.addEventListener("click", function (){
+startButton.addEventListener("click", function () {
     nextQuestion();
 });
 
-function userChoiceAction (answerButton){
-    console.log(currentQuestion);
+function removeEventListeners() {
+    answer1.removeEventListener("click", clickAnswer1);
+    answer2.removeEventListener("click", clickAnswer2);
+    answer3.removeEventListener("click", clickAnswer3);
+    answer4.removeEventListener("click", clickAnswer4);
+}
+
+function userChoiceAction (answerButton) {
     if (currentQuestion >= questions.length - 1) {
-        showScore()
-    }else{
+        showScoreInput()
+    } 
+    else {
         userAnswers.push(answerButton.textContent);
         nextQuestion();
     }
 }
 
-function clickAnswer () {
-    console.log("button" + currentButton);
-    currentButton++;
-    userChoiceAction (ans[currentButton]);
+function clickAnswer1() {
+    userChoiceAction (ans[0]);
 }
 
-function showScore () {
+function clickAnswer2() {
+    userChoiceAction (ans[1]);
+}
+
+function clickAnswer3() {
+    userChoiceAction (ans[2]);
+}
+
+function clickAnswer4() {
+    userChoiceAction (ans[3]);
+}
+
+
+function showScoreInput () {
     var questionDiv = document.querySelector("#question");
     questionDiv.remove();
-    var score = 0;
+
+
     for(var i = 0; i < questions.length; i++) {
-        if (questions[i].answer===userAnswers[i]){
+        if (questions[i].answer === userAnswers[i]){
             score = score + 5;
         }
-        console.log(questions[i].answer + " - " + userAnswers[i]);
     } 
-    var scorePage = document.querySelector(".score-page");
-    scorePage.innerHTML += "Your score is " + score;
+
+
+    var inp = document.createElement("input");
+
+    inp.addEventListener("keydown", function(key) {
+        if(key.code === "Enter") {
+           initials = document.querySelector("input").value;
+           showScore();
+        }
+    });
+
+    scorePage.appendChild(inp);
+    scorePage.style.display = "flex";
+
+}
+
+function showScore() {
+    scorePage.innerHTML = "Your score is " + score + "  " + initials;
+    document.querySelector('#restart-button').addEventListener('click', function(){
+        window.location.reload();
+        return false;
+    });   
+
+    const timeH = document.querySelector("h2");
+    let timeSecond = 75;
+
+    timeH.innerHTML = "00:${timeSecond}";
+
+    const countDown = setInterval (()=>{
+        timeSecond--; 
+        if(timeSecond <= 0 || timeSecond < 1){
+            clearInterval(countDown);
+        }
+    },1000);
 }
 
 
@@ -114,30 +174,6 @@ function showScore () {
 
 
 
-
-// var newQuestSection = document.querySelector(".question");
-// var qNumber = 0;
-
-// startButton.addEventListener("click", function(){
-//     document.querySelector(".center").style.display = "none";
-//     makeQuestion(qNumber);
-// })
-
-// function makeQuestion (questionNumber) {
-//     var quest0 = document.querySelector("div#newQuest0 > h2");
-//     quest0.textContent = questions[questionNumber].questionTitle; 
- 
-//     for (var i=0; i < 4; i++) {
-//         var choiceButton = document.createElement("button");
-//         choiceButton.textContent = questions[questionNumber].choices[i];
-//         choiceButton.addEventListener("click",   function(){
-//             newQuestSection.textContent = "";
-//             makeQuestion(qNumber);
-//         })
-//         newQuestSection.appendChild(choiceButton);
-//     } 
-//     qNumber += 1;
-// }
 
 
 
